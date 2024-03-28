eede071945a6899b237b60a0149e2e8575e358ac
CoolShivas <shivas2710cool00@gmail.com>
2024-03-28 16:19:05 +0530

    React_Router_Dom

--- ecommerce_website/src/App.jsx
+++ ecommerce_website/src/App.jsx
@@ -1,5 +1,27 @@
+import ErrorPage from "./components/Nabar/ErrorPage";
+import SingleProduct from "./components/Nabar/SingleProduct";
+import Cart from "./components/Nabar/Cart";
+import Products from "./components/Nabar/Products";
+import ContactPage from "./components/Nabar/ContactPage";
+import AboutPage from "./components/Nabar/AboutPage";
+import HomePage from "./components/Nabar/HomePage";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+
+
+
 function App() {
-  return <h1> Shiva's Ecommerce Website </h1>
+  return <Router>
+    <Routes>
+      <Route path="/" Component={HomePage}></Route>
+      <Route path="/about" Component={AboutPage}></Route>
+      <Route path="/contact" Component={ContactPage}></Route>
+      <Route path="/products" Component={Products}></Route>
+      <Route path="/cart" Component={Cart}></Route>
+      <Route path="/singleproducts/:id" Component={SingleProduct}></Route>
+      <Route path="*" Component={ErrorPage}></Route>
+    </Routes>
+  </Router>
 }
 
 export default App;
