9863ec43ed85d5836ca86965c5a1d5a602cf1db8
CoolShivas <shivas2710cool00@gmail.com>
2024-03-30 00:46:45 +0530

    CartIcon_NavBar_CSS

--- ecommerce_website/src/components/Header/Header.jsx
+++ ecommerce_website/src/components/Header/Header.jsx
@@ -1,7 +1,8 @@
+import classes from "./Header.module.css";
 import Navbar from "../Nabar/Navbar"
 
 const Header = () => {
-    return <div className="main_header">
+    return <div className={classes.main_header}>
         <h1> Main Header </h1>
         <Navbar></Navbar>
     </div>
--- ecommerce_website/src/components/Nabar/Navbar.jsx
+++ ecommerce_website/src/components/Nabar/Navbar.jsx
@@ -1,33 +1,39 @@
+import { ImCart } from "react-icons/im";
+import classes from "./Navbar.module.css";
 import { NavLink } from "react-router-dom"
 
 const Navbar = () => {
     return <>
-        <nav>
-            <ul className="ulTag">
-                <li>
-                    <NavLink to="/"> Home </NavLink>
-                </li>
+        <nav className={classes.nav}>
+            <div className={classes.navbar}>
+                <ul className={classes.navbar_list}>
+                    <li>
+                        <NavLink to="/"> Home </NavLink>
+                    </li>
 
-                <li>
-                    <NavLink to="/about"> About Us </NavLink>
-                </li>
+                    <li>
+                        <NavLink to="/about"> About Us </NavLink>
+                    </li>
 
-                <li>
-                    <NavLink to="/contact"> Contact Us </NavLink>
-                </li>
+                    <li>
+                        <NavLink to="/contact"> Contact Us </NavLink>
+                    </li>
 
-                <li>
-                    <NavLink to="/singleproducts/:id"> Single Product </NavLink>
-                </li>
+                    <li>
+                        <NavLink to="/singleproducts/:id"> Single Product </NavLink>
+                    </li>
 
-                <li>
-                    <NavLink to="cart/"> Cart </NavLink>
-                </li>
+                    <li>
+                        {/* <NavLink to="cart/"> Cart </NavLink> */}
+                        <NavLink to="cart/" className={classes.cart_trolley}> <ImCart /> </NavLink>
+                    </li>
 
-                <li>
-                    <NavLink to="/products"> Products </NavLink>
-                </li>
-            </ul>
+                    <li>
+                        <NavLink to="/products"> Products </NavLink>
+                    </li>
+                </ul>
+            </div>
+
         </nav>
     </>
 }
