c78aa17fa0361a96625ff7eccca5220f98a5afdc
CoolShivas <shivas2710cool00@gmail.com>
2024-03-30 10:22:13 +0530

    CSS_of_Navbar_Header

--- ecommerce_website/src/components/Nabar/Navbar.jsx
+++ ecommerce_website/src/components/Nabar/Navbar.jsx
@@ -1,41 +1,51 @@
+import { GrClose } from "react-icons/gr";
+import { TiThMenu } from "react-icons/ti";
 import { ImCart } from "react-icons/im";
 import classes from "./Navbar.module.css";
 import { NavLink } from "react-router-dom"
 
 const Navbar = () => {
-    return <>
-        <nav className={classes.nav}>
-            <div className={classes.navbar}>
-                <ul className={classes.navbar_list}>
-                    <li>
-                        <NavLink to="/"> Home </NavLink>
-                    </li>
+    return <div className={classes.navbar}>
+        <ul className={classes.navbar_list}>
 
-                    <li>
-                        <NavLink to="/about"> About Us </NavLink>
-                    </li>
+            <li>
+                <NavLink to="/" className={classes.navbar_link}> Home </NavLink>
+            </li>
 
-                    <li>
-                        <NavLink to="/contact"> Contact Us </NavLink>
-                    </li>
+            <li>
+                <NavLink to="/about" className={classes.navbar_link}> About Us </NavLink>
+            </li>
 
-                    <li>
-                        <NavLink to="/singleproducts/:id"> Single Product </NavLink>
-                    </li>
+            <li>
+                <NavLink to="/contact" className={classes.navbar_link}> Contact Us </NavLink>
+            </li>
 
-                    <li>
-                        {/* <NavLink to="cart/"> Cart </NavLink> */}
-                        <NavLink to="cart/" className={classes.cart_trolley}> <ImCart /> </NavLink>
-                    </li>
+            <li>
+                <NavLink to="/singleproducts/:id" className={classes.navbar_link}> Single Product </NavLink>
+            </li>
 
-                    <li>
-                        <NavLink to="/products"> Products </NavLink>
-                    </li>
-                </ul>
-            </div>
+            <li>
+                <NavLink to="cart/"
+                    className={classes.navbar_link['cart_trolley__link']}>
+                    <ImCart className={classes.cart_trolley} />
+                    <span className={classes.navbar_link['cart_total__item']} > 50 </span>
+                </NavLink>
+            </li>
 
-        </nav>
-    </>
+            <li>
+                <NavLink to="/products" className={classes.navbar_link}> Products </NavLink>
+            </li>
+        </ul>
+
+        <div className={classes.mobile_navbar_btn}>
+
+            <TiThMenu name="menu_outline" className={classes.mobile_nav_icon} />
+
+            <GrClose name="close_outline" className={classes.mobile_nav_icon['close_outline']} />
+        </div>
+
+    </div>
+
 }
 
 export default Navbar
