7e7cdb02463c9a13e4bc359c559782ac152739e2
CoolShivas <shivas2710cool00@gmail.com>
2024-04-13 02:31:45 +0530

    HeroSection_with_Img_Para_using_Grid

---
+++ ecommerce_website/src/components/Header/HeroSection.jsx
@@ -0,0 +1,29 @@
+import classes from "./HeroSection.module.css";
+
+const HeroSection = () => {
+  return (
+    <div className={classes.container}>
+      <div className={classes.grid_two__columns}>
+        <div className={classes.hero_section__data}>
+          <p className={classes.intro_data}> Welcome to </p>
+          <h1> Shiva Classes </h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum,
+            molestiae voluptatem qui hic enim eum ab, perferendis eos similique
+            tempore placeat eaque mollitia quidem alias.
+          </p>
+        </div>
+        <div>
+          <figure>
+            <img
+              src="../../../images/Beautiful Way.jpg"
+              alt="image not found"
+            />
+          </figure>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default HeroSection;
--- ecommerce_website/src/components/Nabar/HomePage.jsx
+++ ecommerce_website/src/components/Nabar/HomePage.jsx
@@ -1,7 +1,12 @@
-
+import HeroSection from "../Header/HeroSection";
 
 const HomePage = () => {
-    return <h2> This is a home page. </h2>
-}
+  return (
+    <>
+      <h2> This is a home page. </h2>
+      <HeroSection></HeroSection>
+    </>
+  );
+};
 
-export default HomePage
+export default HomePage;
