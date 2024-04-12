c383421164209daddced9f4f36f98d8e43a28997
CoolShivas <shivas2710cool00@gmail.com>
2024-04-13 00:04:04 +0530

    Navbar_with_Humburger_Close_Icon_Active_Class

--- ecommerce_website/src/components/Nabar/Navbar.jsx
+++ ecommerce_website/src/components/Nabar/Navbar.jsx
@@ -2,48 +2,65 @@ import { GrClose } from "react-icons/gr";
 import { TiThMenu } from "react-icons/ti";
 import { ImCart } from "react-icons/im";
 import classes from "./Navbar.module.css";
-import { NavLink } from "react-router-dom"
+import { NavLink } from "react-router-dom";
+import { useState } from "react";
 
 const Navbar = () => {
-    return <div className={classes.navbar}>
-        <ul className={classes.navbar_list}>
+  const [menuIcon, setMenuIcon] = useState();
 
-            <li>
-                <NavLink to="/" className={classes.navbar_link}> Home </NavLink>
-            </li>
+  return (
+    <div className={menuIcon ? "navbar active" : "navbar"}>
+      <ul className={classes.navbar_lists}>
+        <li>
+          <NavLink to="/" className={classes.navbar_link}>
+            Home
+          </NavLink>
+        </li>
 
-            <li>
-                <NavLink to="/about" className={classes.navbar_link}> About </NavLink>
-            </li>
+        <li>
+          <NavLink to="/about" className={classes.navbar_link}>
+            About
+          </NavLink>
+        </li>
 
-            <li>
-                <NavLink to="/contact" className={classes.navbar_link}> Contact </NavLink>
-            </li>
+        <li>
+          <NavLink to="/contact" className={classes.navbar_link}>
+            Contact
+          </NavLink>
+        </li>
 
-            <li>
-                <NavLink to="/products" className={classes.navbar_link}> Products </NavLink>
-            </li>
+        <li>
+          <NavLink to="/products" className={classes.navbar_link}>
+            Products
+          </NavLink>
+        </li>
 
-            <li>
-                <NavLink to="cart/"
-                    className={classes.navbar_link['cart_trolley__link']}>
-                    <ImCart className={classes.cart_trolley} />
-                    <span className={classes.navbar_link['cart_total__item']} > 50 </span>
-                </NavLink>
-            </li>
+        <li>
+          <NavLink
+            to="cart/"
+            className={classes.navbar_link["cart_trolley__link"]}
+          >
+            <ImCart className={classes.cart_trolley} />
+            <span className={classes.navbar_link["cart_total__item"]}>50</span>
+          </NavLink>
+        </li>
+      </ul>
 
+      <div className={classes.mobile_navbar_btn}>
+        <TiThMenu
+          name="menu_outline"
+          className={classes.mobile_nav_icon}
+          onClick={() => setMenuIcon(false)}
+        />
 
-        </ul>
-
-        <div className={classes.mobile_navbar_btn}>
-
-            <TiThMenu name="menu_outline" className={classes.mobile_nav_icon} />
-
-            <GrClose name="close_outline" className={classes.mobile_nav_icon['close_outline']} />
-        </div>
-
+        <GrClose
+          name="close_outline"
+          className={classes.mobile_nav_icon__close_outline}
+          onClick={() => setMenuIcon(true)}
+        />
+      </div>
     </div>
+  );
+};
 
-}
-
-export default Navbar
+export default Navbar;
