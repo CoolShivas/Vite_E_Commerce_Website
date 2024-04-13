24aa91f7df57e4ee7a33181b959f171c4495144d
CoolShivas <shivas2710cool00@gmail.com>
2024-04-14 02:25:09 +0530

    ContactPage_completed_using_GoogleMap_Formspree

--- ecommerce_website/src/components/Nabar/ContactPage.jsx
+++ ecommerce_website/src/components/Nabar/ContactPage.jsx
@@ -1,7 +1,57 @@
-
+import classes from "./ContactPage.module.css";
 
 const ContactPage = () => {
-    return <h2> This is a contact page. </h2>
-}
+  return (
+    <>
+      <h2 className={classes.common_heading}> Feel Free to Contact Us </h2>
+      <iframe
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d14504.45127810879!2d77.30781526384891!3d24.654244699025675!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x397a5c70d5839ad3%3A0xc46638d03224b797!2sPiproda%20Khurd%2C%20Madhya%20Pradesh%20473001!5e0!3m2!1sen!2sin!4v1713036308587!5m2!1sen!2sin"
+        width="100%"
+        height="400"
+        style={{ border: 0 }}
+        allowFullScreen=""
+        loading="lazy"
+        referrerPolicy="no-referrer-when-downgrade"
+      ></iframe>
 
-export default ContactPage
+      <div className={classes.container}>
+        <div className={classes.contact_form}>
+          <form
+            action="https://formspree.io/f/xkndpyej"
+            method="POST"
+            className={classes.contact_inputs}
+          >
+            <input
+              type="text"
+              placeholder="username"
+              name="UsrName"
+              required
+              autoComplete="off"
+            />
+
+            <input
+              type="email"
+              name="Email"
+              placeholder="Email"
+              autoCapitalize="off"
+              required
+            />
+
+            <textarea
+              name="message"
+              cols="30"
+              rows="10"
+              required
+              autoComplete="off"
+              placeholder="Enter your message her."
+            ></textarea>
+
+            <button type="submit">Send</button>
+          </form>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default ContactPage;
