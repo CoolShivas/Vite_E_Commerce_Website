6f3715e21f7520f859c11b9e471e56e65b0cffa6
CoolShivas <shivas2710cool00@gmail.com>
2024-04-15 17:57:26 +0530

    UseEffect_Hook_to_fetchData_with_helpOf_Axios

--- ecommerce_website/src/components/Header/Header.jsx
+++ ecommerce_website/src/components/Header/Header.jsx
@@ -1,13 +1,9 @@
 import classes from "./Header.module.css";
 import Navbar from "../Nabar/Navbar";
-import { formationOfCustomHook } from "../../store/AppContext";
 
 const Header = () => {
-  const { myName } = formationOfCustomHook();
-
   return (
     <div className={classes.main_header}>
-      <h1> {myName} </h1>
       <h1> Main Header </h1>
       <Navbar></Navbar>
     </div>
--- ecommerce_website/src/store/AppContext.jsx
+++ ecommerce_website/src/store/AppContext.jsx
@@ -1,10 +1,26 @@
-import { createContext, useContext } from "react";
-
+import axios from "axios";
+import { createContext, useContext, useEffect } from "react";
+// Starting or creation of CreateContext
 const AppContext = createContext();
 
 export default AppContext;
+// Ending or creation of CreateContext
+
+// Starting or creation of Context Provider
+const API = "https://api.pujakaitem.com/api/products";
 
 const ContextProvider = ({ children }) => {
+  const getProducts = async (url) => {
+    const response = await axios.get(url);
+    // console.log(response);
+    const production = await response.data;
+    console.log(production);
+  };
+
+  useEffect(() => {
+    getProducts(API);
+  }, []);
+
   return (
     <AppContext.Provider value={{ myName: "Shiva Chouhan" }}>
       {children}
@@ -13,9 +29,12 @@ const ContextProvider = ({ children }) => {
 };
 
 export { ContextProvider };
+// Ending or creation of Context Provider
 
+// Starting or creation of Custom Hook
 const formationOfCustomHook = () => {
   return useContext(AppContext);
 };
 
 export { formationOfCustomHook };
+// Endng or creation of Custom Hook
