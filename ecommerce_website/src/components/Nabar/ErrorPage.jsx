d1a5cd02d055e526888057f2134d1c9a73138b73
CoolShivas <shivas2710cool00@gmail.com>
2024-04-15 15:36:13 +0530

    ErrorPage_Completed

--- ecommerce_website/src/components/Nabar/ErrorPage.jsx
+++ ecommerce_website/src/components/Nabar/ErrorPage.jsx
@@ -1,7 +1,22 @@
-
+import { NavLink } from "react-router-dom";
+import classes from "./ErrorPage.module.css";
 
 const ErrorPage = () => {
-    return <h2> Something went wrong !</h2>
-}
+  return (
+    <div className={classes.error_page}>
+      <h2>404</h2>
+      <h3> UH OH ! You're lost.</h3>
+      <p>
+        The page you are looking for does not exist. How you got here is a
+        mystery. But you can click on the button below to go back to the
+        homepage.
+      </p>
 
-export default ErrorPage
+      <NavLink to="/">
+        <button> go back to home </button>
+      </NavLink>
+    </div>
+  );
+};
+
+export default ErrorPage;
