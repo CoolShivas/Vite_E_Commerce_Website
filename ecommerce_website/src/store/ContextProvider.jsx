2835f19ee5de0fcf08c6e222edcc2e498384b093
CoolShivas <shivas2710cool00@gmail.com>
2024-04-16 08:23:13 +0530

    Use_of_UseReducer_with_initialState_and_dispatchFunction

--- ecommerce_website/src/store/ContextProvider.jsx
+++ ecommerce_website/src/store/ContextProvider.jsx
@@ -1,19 +1,64 @@
 import axios from "axios";
-import { useEffect } from "react";
+import { useEffect, useReducer } from "react";
 import AppContext from "./AppContext";
 
 // Starting or creation of Context Provider
 const API = "https://api.pujakaitem.com/api/products";
 
+const initialState = {
+  isLoading: false,
+  isError: false,
+  products: [],
+  featureProducts: [],
+};
+
+const pureFuncForReducer = (currState, action) => {
+  if (action.type === "SET_LOADING") {
+    return {
+      ...currState,
+      isLoading: true,
+    };
+  } else if (action.type === "SET_API_DATA") {
+    const featureData = action.payload.filter((currElem) => {
+      return currElem.featured === true;
+    });
+    return {
+      ...currState,
+      isLoading: false,
+      products: action.payload,
+      featureProducts: featureData,
+    };
+  } else {
+    return {
+      ...currState,
+      isLoading: false,
+      isError: true,
+    };
+  }
+};
+
 const ContextProvider = (props) => {
+  const [newData, dispatchData] = useReducer(pureFuncForReducer, initialState);
+
   const getProducts = async (url) => {
+    dispatchData({
+      type: "SET_LOADING",
+    });
+
     try {
       const response = await axios.get(url);
       // console.log(response);
-      const production = await response.data;
-      console.log(production);
+      const productResponse = await response.data;
+      // console.log(production);
+      dispatchData({
+        type: "SET_API_DATA",
+        payload: productResponse,
+      });
     } catch (error) {
       console.log(error);
+      dispatchData({
+        type: "API_ERROR",
+      });
     }
   };
 
@@ -22,7 +67,7 @@ const ContextProvider = (props) => {
   }, []);
 
   return (
-    <AppContext.Provider value={{ myName: "Shiva Chouhan" }}>
+    <AppContext.Provider value={{ ...newData }}>
       {props.children}
     </AppContext.Provider>
   );
