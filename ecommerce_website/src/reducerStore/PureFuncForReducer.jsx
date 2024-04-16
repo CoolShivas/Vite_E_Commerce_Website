de3315784fea2fa2516e047dff47755ef072039b
CoolShivas <shivas2710cool00@gmail.com>
2024-04-16 08:49:25 +0530

    Use_of_Switch_Statement_in_PureFuncForReducer

--- ecommerce_website/src/reducerStore/PureFuncForReducer.jsx
+++ ecommerce_website/src/reducerStore/PureFuncForReducer.jsx
@@ -1,25 +1,56 @@
 const PureFuncForReducer = (currState, action) => {
-  if (action.type === "SET_LOADING") {
-    return {
-      ...currState,
-      isLoading: true,
-    };
-  } else if (action.type === "SET_API_DATA") {
-    const featuredData = action.payload.filter((currElement) => {
-      return currElement.featured === true;
-    });
-    return {
-      ...currState,
-      isLoading: false,
-      products: action.payload,
-      featureProducts: featuredData,
-    };
-  } else if (action.type === "API_ERROR") {
-    return {
-      ...currState,
-      isLoading: false,
-      isError: true,
-    };
+  //   if (action.type === "SET_LOADING") {
+  //     return {
+  //       ...currState,
+  //       isLoading: true,
+  //     };
+  //   } else if (action.type === "SET_API_DATA") {
+  //     const featuredData = action.payload.filter((currElement) => {
+  //       return currElement.featured === true;
+  //     });
+  //     return {
+  //       ...currState,
+  //       isLoading: false,
+  //       products: action.payload,
+  //       featureProducts: featuredData,
+  //     };
+  //   } else if (action.type === "API_ERROR") {
+  //     return {
+  //       ...currState,
+  //       isLoading: false,
+  //       isError: true,
+  //     };
+  //   }
+
+  // Instead of If else conditions using Switch Statements
+
+  switch (action.type) {
+    case "SET_LOADING":
+      return {
+        ...currState,
+        isLoading: true,
+      };
+
+    case "SET_API_DATA":
+      const featuredData = action.payload.filter((currElement) => {
+        return currElement.featured === true;
+      });
+      return {
+        ...currState,
+        isLoading: false,
+        products: action.payload,
+        featureProducts: featuredData,
+      };
+
+    case "API_ERROR":
+      return {
+        ...currState,
+        isLoading: false,
+        isError: true,
+      };
+
+    default:
+      return currState;
   }
 };
 
