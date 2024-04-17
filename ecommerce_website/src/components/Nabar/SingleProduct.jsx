188179d0c8d1715546e6633439f6f1f083062cb7
CoolShivas <shivas2710cool00@gmail.com>
2024-04-17 10:39:02 +0530

    Access_SingleProduct_Details_by_UseParams_Hook

--- ecommerce_website/src/components/Nabar/SingleProduct.jsx
+++ ecommerce_website/src/components/Nabar/SingleProduct.jsx
@@ -1,18 +1,21 @@
+import { useParams } from "react-router-dom";
 import { formationOfCustomHook } from "../../store/AppContext";
 import { useEffect } from "react";
 
 const SingleProduct = () => {
-  const { getSingleProduct } = formationOfCustomHook();
+  const { id } = useParams();
+  console.log(id);
+
+  const { getSingleProduct, isSingleLoading, singleProduct } =
+    formationOfCustomHook();
   console.log(getSingleProduct); // // Here, we are able to access the single product function;
+  console.log(singleProduct); // // Here, we are able to access the single product function only but we want the specific product details. For that we have to use the useParam hook for that ;
 
   useEffect(() => {
-    getSingleProduct(
-      "https://api.pujakaitem.com/api/products?id=thapaseialnoa"
-    );
+    getSingleProduct(`https://api.pujakaitem.com/api/products?id=${id}`);
   }, []);
 
   return <h2> Unique single products are here. </h2>;
 };
 
 export default SingleProduct;
-// `https://api.pujakaitem.com/api/products?id=thapaseialnoa`;
