852c40c7d982c6c2b9f15b92206e1a865d5ceb85
CoolShivas <shivas2710cool00@gmail.com>
2024-04-24 00:40:31 +0530

    Alignment_of_Images_of_SingleProduct

--- ecommerce_website/src/components/Ui/ImgPicture.jsx
+++ ecommerce_website/src/components/Ui/ImgPicture.jsx
@@ -1,13 +1,31 @@
-const ImgPicture = ({picture}) => {
+import classes from "./ImgPicture.module.css";
+
+const ImgPicture = ({ picture }) => {
   // console.log(picture); // Here, we are getting the images;
-  return <>
-  {picture.map((drr, index)=>{
-    return <figure>
-      <img src={drr.url} alt={drr.filename} className="box_image__style"
-      key={index} />
-    </figure>
-  })}
-  </>
+  return (
+    <div className={classes.grid}>
+      <div className={classes.grid_four__column}>
+        {picture.map((drr, index) => {
+          return (
+            <figure>
+              <img
+                src={drr.url}
+                alt={drr.filename}
+                className="box_image__style"
+                key={index}
+              />
+            </figure>
+          );
+        })}
+      </div>
+
+      {/* Starting of Second Column */}
+      <div className={classes.main_screen}>
+        <img src={picture[0].url} alt={picture[0].filename} />
+      </div>
+      {/* Ending of Second Column */}
+    </div>
+  );
 };
 
 export default ImgPicture;
