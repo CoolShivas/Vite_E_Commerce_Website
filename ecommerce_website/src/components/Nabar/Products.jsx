527c7c24bdf7b5dcef399452710be3922b90e008
CoolShivas <shivas2710cool00@gmail.com>
2024-05-03 15:52:51 +0530

    Product Page with all Filters & Sorting Components

--- ecommerce_website/src/components/Nabar/Products.jsx
+++ ecommerce_website/src/components/Nabar/Products.jsx
@@ -1,7 +1,27 @@
+import ProductList from "../Products-Components/ProductList";
+import Sort from "../Products-Components/Sort";
+import FilterSection from "../Products-Components/FilterSection";
+import classes from "./Product.module.css";
 
 
 const Products = () => {
-    return <h2> Lots of products are available here. </h2>
+    return <>
+    <h2> Lots of products are available here. </h2>
+    <div className={classes.grid_filter__column}>
+        <div>
+            <FilterSection></FilterSection>
+        </div>
+        <section className="product_view__sort">
+        <div className="sort_filter">
+            <Sort></Sort>
+        </div>
+        <div className="main_product">
+            <ProductList></ProductList>
+        </div>
+        </section>
+
+    </div>
+    </>
 }
 
-export default Products
+export default Products;
