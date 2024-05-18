5014f6f970c00cc0e9c46c12a956c8b260acee96
CoolShivas <shivas2710cool00@gmail.com>
2024-05-18 18:23:41 +0530

    Shortten the Functionality added to the Sorting the price of products by filtering the price lower to hightest as well as ascending and descending order

--- ecommerce_website/src/reducerStore/reducerFilter/FilterFuncForReducer.jsx
+++ ecommerce_website/src/reducerStore/reducerFilter/FilterFuncForReducer.jsx
@@ -31,37 +31,64 @@ const FilterFuncForReducer = (currFilterState, action) => {
         sorting_price: action.payload,
       }
 
+    // Sorting the products on the basis of price from highest to lowest;
+    // case "SORTING_PRODUCT_PRICE":
+    //   let latestSortData;
+    //   let tempSortData = [...action.payload];
+
+    //   // Sorting the product ascendingly on the basis of alphabets;
+    //   if (currFilterState.sorting_price === "a-z") {
+    //     latestSortData = tempSortData.sort((a, b) => {
+    //       return a.name.localeCompare(b.name);
+    //     })
+    //   }
+    //   // Sorting the product decendingly on the basis of alphabets;
+    //   if (currFilterState.sorting_price === "z-a") {
+    //     latestSortData = tempSortData.sort((a, b) => {
+    //       return b.name.localeCompare(a.name);
+    //     })
+    //   }
+    //   // Sorting the product ascendingly on the basis of price;
+    //   if (currFilterState.sorting_price === "lowest") {
+    //     const sortingProLowest = ((a, b) => {
+    //       return a.price - b.price;
+    //     })
+    //     latestSortData = tempSortData.sort(sortingProLowest);
+    //   }
+    //   // Sorting the product ascendingly on the basis of price;
+    //   if (currFilterState.sorting_price === "highest") {
+    //     const sortingProHighest = ((a, b) => {
+    //       return b.price - a.price;
+    //     })
+    //     latestSortData = tempSortData.sort(sortingProHighest);
+    //   }
+    // return {
+    //   ...currFilterState,
+    //   filter_products: latestSortData,
+    // }
+
+    // Sorting the products on the basis of price from highest to lowest;
     case "SORTING_PRODUCT_PRICE":
+
       let latestSortData;
-      let tempSortData = [...action.payload];
+      const { filter_products, sorting_price } = currFilterState;
+      let tempSortProduct = [...filter_products];
 
-      // Sorting the product ascendingly on the basis of alphabets;
-      if (currFilterState.sorting_price === "a-z") {
-        latestSortData = tempSortData.sort((a, b) => {
-          return a.name.localeCompare(b.name);
-        })
-      }
-      // Sorting the product decendingly on the basis of alphabets;
-      if (currFilterState.sorting_price === "z-a") {
-        latestSortData = tempSortData.sort((a, b) => {
-          return b.name.localeCompare(a.name);
-        })
-      }
-      // Sorting the product ascendingly on the basis of price;
-      if (currFilterState.sorting_price === "lowest") {
-        const sortingProLowest = ((a, b) => {
+      const sortingProductsPrice = (a, b) => {
+        if (sorting_price === "lowest") {
           return a.price - b.price;
-        })
-        latestSortData = tempSortData.sort(sortingProLowest);
-      }
-      // Sorting the product ascendingly on the basis of price;
-      if (currFilterState.sorting_price === "highest") {
-        const sortingProHighest = ((a, b) => {
+        }
+        if (sorting_price === "highest") {
           return b.price - a.price;
-        })
-        latestSortData = tempSortData.sort(sortingProHighest);
-      }
-
+        }
+        if (sorting_price === "a-z") {
+          return a.name.localeCompare(b.name);
+        }
+        if (sorting_price === "z-a") {
+          return b.name.localeCompare(a.name);
+        }
+      };
+      latestSortData = tempSortProduct.sort(sortingProductsPrice);
       return {
         ...currFilterState,
         filter_products: latestSortData,
