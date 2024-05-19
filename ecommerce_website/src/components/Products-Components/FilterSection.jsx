c24d10372308d40545839e3dc045ec4951c59c5e
CoolShivas <shivas2710cool00@gmail.com>
2024-05-19 12:14:31 +0530

    Upgraded the Common Function to get unique data of an object with specific categories

--- ecommerce_website/src/components/Products-Components/FilterSection.jsx
+++ ecommerce_website/src/components/Products-Components/FilterSection.jsx
@@ -8,23 +8,28 @@ const FilterSection = () => {
 
   // // To get the Unique data of each fields;
 
-  const gettingUniqueData = (all_products) => {
-    let newValue = all_products.map((arr) => {
-      return arr.category;
+  const gettingUniqueData = (data, property) => {
+    let newValue = data.map((arr) => {
+      return arr[property];
       // // arr.category will provide all the products categories such as mobile, laptop , watches, etc.
+      // // arr[property] will provide all the categories access for all such mobile category only or laptop category, etc.
+      // // if select the mobile category or laptop category or watch categroy using arr[property];
+      // // Whereas arr.category will provide the array of products categories only;
     })
+    newValue = ["All", ...new Set(newValue)]; // // Here, we are getting the specific categroies only;
+    // // Now, on specific category we are able to select the products accordingly;
     console.log(newValue);
   };
 
 
 
   // // We need Unique Data;
-  const categoryOnlyData = gettingUniqueData(all_products);
+  const categoryOnlyData = gettingUniqueData(all_products, "category");
 
   return (
     <div className={classes.filter_search}>
       <form onSubmit={(e) => { e.preventDefault() }}>
-        <input type="text" name="text" value={text} onChange={updateFilteringBySearch} />
+        <input type="text" name="text" value={text} onChange={updateFilteringBySearch} placeholder="Search" className="form-control" />
       </form>
     </div>
   )
