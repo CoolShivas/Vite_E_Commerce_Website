420f9a65177613dbe9d79d56a644789310a367c4
CoolShivas <shivas2710cool00@gmail.com>
2024-05-19 11:40:32 +0530

    Common Function to get unique data of an object

--- ecommerce_website/src/components/Products-Components/FilterSection.jsx
+++ ecommerce_website/src/components/Products-Components/FilterSection.jsx
@@ -4,7 +4,22 @@ import { useFilterCustomHook } from "../../store/FilterContext";
 
 const FilterSection = () => {
 
-  const { search_filtering: { text }, updateFilteringBySearch } = useFilterCustomHook();
+  const { search_filtering: { text }, updateFilteringBySearch, all_products } = useFilterCustomHook();
+
+  // // To get the Unique data of each fields;
+
+  const gettingUniqueData = (all_products) => {
+    let newValue = all_products.map((arr) => {
+      return arr.category;
+      // // arr.category will provide all the products categories such as mobile, laptop , watches, etc.
+    })
+    console.log(newValue);
+  };
+
+
+
+  // // We need Unique Data;
+  const categoryOnlyData = gettingUniqueData(all_products);
 
   return (
     <div className={classes.filter_search}>
