6ae8b9000e6c425927835fff0a19d0e0a95b1d85
CoolShivas <shivas2710cool00@gmail.com>
2024-05-20 18:32:19 +0530

    Initialization of Cart Reducer

--- ecommerce_website/src/store/CartContext.jsx
+++ ecommerce_website/src/store/CartContext.jsx
@@ -1,14 +1,31 @@
-import React, { Children, createContext } from 'react'
+import React, { createContext, useReducer } from 'react'
 
 const CartContext = createContext();
 
+const addToCartInitialState = {
+    cart: [],
+    total_item: "",
+    total_amount: "",
+    shipping_fee: 50000,
+};
+
 export const CartProvider = ({ children }) => {
 
-    const additionToCart = (id, myColour, amount, productABC) => {
+    const [newCartState, dispatchCartData] = useReducer(CartFuncForReducer, addToCartInitialState);
 
+    const additionToCart = (id, myColour, amount, productABC) => {
+        dispatchCartData({
+            type: "ADD_TO_CART",
+            payload: (id, myColour, amount, productABC)
+        })
     };
 
-    return <CartContext.Provider>
+    return <CartContext.Provider value={
+        {
+            ...newCartState,
+            additionToCart,
+        }
+    }>
         {children}
     </CartContext.Provider>
 }
