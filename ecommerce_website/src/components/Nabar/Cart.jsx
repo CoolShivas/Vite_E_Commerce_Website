34b9b1112b819c4e8e3348a118c5004c3bae23ba
CoolShivas <shivas2710cool00@gmail.com>
2024-05-24 10:52:00 +0530

    Cart Reducer now have cart put items and push into the cart bucket and getting the data on cart component

--- ecommerce_website/src/components/Nabar/Cart.jsx
+++ ecommerce_website/src/components/Nabar/Cart.jsx
@@ -1,6 +1,11 @@
+import { useAddTocartCustomHook } from "../../store/CartContext"
 
 
 const Cart = () => {
+
+    const { cart } = useAddTocartCustomHook();
+    console.log(cart);
+
     return <h2> This is a cart page. </h2>
 }
 
--- ecommerce_website/src/reducerStore/reducerCart/CartFuncForReducer.jsx
+++ ecommerce_website/src/reducerStore/reducerCart/CartFuncForReducer.jsx
@@ -4,7 +4,21 @@ const CartFuncForReducer = (currCartState, action) => {
     if (action.type === "ADD_TO_CART") {
         let { id, myColour, amount, productABC } = action.payload;
         console.log(productABC);
+
+        let cartPutItems = {
+            id: id + myColour,
+            name: productABC.name,
+            colors: myColour,
+            amount: amount,
+            price: productABC.price,
+            image: productABC.image[0].url,
+            max: productABC.stock,
+        };
+        return { ...currCartState, cart: [...currCartState.cart, cartPutItems] }
+
     }
+
+
     return currCartState;
 }
 
